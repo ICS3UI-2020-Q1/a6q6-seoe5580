1ff7bce10f8c0087c235b639a86eafb707c3b72e
seoe5580 <seoe5580@wrdsb.ca>
2020-10-13 05:30:54 +0000

    Done Fibonacci sequence question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Fibonacci sequence
+ * @author Eric
  */
 public class Main {
 
@@ -9,7 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user what number they want to find
+    System.out.println("What Fibonacci number would you like to find");
+
+    // gets the number from the user
+    int numFind = input.nextInt();
+
+    // sets up an array and sets the store value to numfind
+    int[] num = new int[numFind];
+
+    // declares variables for future calculations
+    int sum = 0;
+    int num2 = 0;
+    int num3 = 1;
+
+    // adds num2 and num3 and gets the sum and repeats until i reaches the user's input
+    for(int i = 0; i < num.length; i++){
+      sum = num2 + num3;
+      num2 = num3;
+      num3 = sum;
+    }
+
+    // tells the user the answer
+    System.out.println("The " + numFind + " Fibonacci number is " + num2);
   }
 }
